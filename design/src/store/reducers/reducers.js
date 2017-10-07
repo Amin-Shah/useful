842bcfd7082078058c8f0641ab0d606fd7710ecb
Amin Shah <amin@abc.com>
2017-10-08 03:00:23 +0500

    initial commit

--- design/src/store/reducers/reducers.js
+++ design/src/store/reducers/reducers.js
@@ -2,34 +2,19 @@ import ActionTypes from '../actions/actionTypes'
 
 const initialState = {};
 
-export function AppReducers(state = initialState, action){
-    switch(action.type){
+export function AppReducers(state = initialState, action) {
+    switch (action.type) {
         case ActionTypes.LOGINREQUESTSUCCESS: {
-            var newState = Object.assign({}, state, {login: action.data})
+            var newState = Object.assign({}, state, { login: action.data })
             state = newState;
             return state;
         }
         case ActionTypes.SIGNUPREQUESTSUCCESS: {
-            var newState = Object.assign({}, state, {signup: action.data})
+            var newState = Object.assign({}, state, { signup: action.data })
             state = newState;
             return state;
         }
-        case ActionTypes.ADDREQUESTSUCCESS: {
-            var newState = Object.assign({}, state, {addItem: action.data})
-            state = newState;
-            return state;
-        }
-        case ActionTypes.VIEWREQUESTSUCCESS: {
-            var newState = Object.assign({}, state, {viewItem: action.data})
-            state = newState;
-            return state;
-        }
-        case ActionTypes.SEARCHREQUESTSUCCESS: {
-            var newState = Object.assign({}, state, {searchItem: action.data})
-            state = newState;
-            return state;
-        }
-        default: 
+        default:
             return state
     }
 }
